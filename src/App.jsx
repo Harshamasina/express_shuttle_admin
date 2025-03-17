36bd95051296576a282e6e47c2c6d704557eae99
Harshamasina <harshamasina1999@gmail.com>
2025-03-17 14:17:19 -0500

    added: "Session Log Out"

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-
+import SessionTimeOut from './Components/SessionTimeOut';
 
 const App = () => {
     return (
         <>
             <BrowserRouter>
                 <Navbar />
+                <SessionTimeOut />
                 <Footer />
             </BrowserRouter>
         </>
